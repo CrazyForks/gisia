75bc25c33f632a9e167bb985a8bace90c51fc0ef
Martin Tan <mtanpub@gmail.com>
2026-02-11 10:46:25 +0800

    change name for branch push

--- lib/gitlab/data_builder/push.rb
+++ lib/gitlab/data_builder/push.rb
@@ -4,6 +4,9 @@
 # Contains code from GitLab FOSS (MIT Licensed)
 # Copyright (c) GitLab Inc.
 # See .licenses/Gisia/others/gitlab-foss.dep.yml for full license
+#
+# Modifications and additions copyright (c) 2025-present Liuming Tan
+# Licensed under AGPLv3 - see LICENSE file in this repository
 # ======================================================
 
 module Gitlab
@@ -123,7 +126,7 @@ module Gitlab
           end
         end
 
-        type = Gitlab::Git.tag_ref?(ref) ? 'tag_push' : 'push'
+        type = Gitlab::Git.tag_ref?(ref) ? 'tag_push' : 'branch_push'
 
         # Hash to be passed as post_receive_data
         {
