d57f19fda3997a097d27abeb76c826cf3f45027e
Martin Tan <mtanpub@gmail.com>
2026-03-23 11:19:18 +0800

    use smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :letter_opener_web
+  if ENV['DEV_SMTP'] == '1'
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  else
+    config.action_mailer.delivery_method = :letter_opener_web
+  end
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   # Make template changes take effect immediately.
