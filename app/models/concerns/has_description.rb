20ae43ea4decedbbc8b1ad909f17311d84435dcc
Martin Tan <mtanpub@gmail.com>
2026-04-21 14:05:29 +0800

    fix multiple line break chars in the editor

--- app/models/concerns/has_description.rb
+++ app/models/concerns/has_description.rb
@@ -4,10 +4,15 @@ module HasDescription
   extend ActiveSupport::Concern
 
   included do
+    before_validation :normalize_description_line_endings
     before_validation :convert_description_to_html
   end
 
   private
+
+  def normalize_description_line_endings
+    self.description = description.gsub("\r\n", "\n") if description.present?
+  end
 
   def convert_description_to_html
     return unless description_changed? && description.present?
